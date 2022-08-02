06b592011d475515e4dfadf08acc0a094e803297
Maayanalt <maayanalt@gmail.com>
2022-08-02 21:20:26 +0300

    edited to use generator of js

--- src/publishers/primesGenerator.js
+++ src/publishers/primesGenerator.js
@@ -5,21 +5,27 @@ class PrimesGenerator extends MyEvent {
     super();
   }
 
-  start(num) {
-    let count = 1;
-    for (let i = 2; i <= num; i++) {
-      let isPrime = true;
-      for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-          isPrime = false;
-          break;
-        }
-      }
-      if (isPrime) {
-        setTimeout(() => this.emit("prime", i), count * 1000);
-        count++;
-      }
+  *generatorPrime(num) {
+    yield 2;
+    for (let i = 3; i <= num; i = i + 2) {
+      if (this.primeCheck(i)) yield i;
     }
+  }
+
+  start(num) {
+    const generator = this.generatorPrime(num);
+    const IntervalID = setInterval(() => {
+      const current = generator.next().value;
+      if (current) this.emit("prime", current);
+      else clearInterval(IntervalID);
+    }, 1000);
+  }
+
+  primeCheck(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
   }
 }
 
